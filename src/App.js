b14cfba24d24e8b485bba07558612ae113ec70b7
BenKamotho <105359629+BernardKamotho@users.noreply.github.com>
2025-03-24 16:18:59 +0300

    Attached the whole app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Getproducts from './components/Getproducts';
+import"bootstrap/dist/js/bootstrap.min.js";
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Navbar from './components/Navbar';
+import Addproducts from './components/Addproducts';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+        <Router>
+          <div className="App backstyling">
+            <header className="App-header">
+              <h1>Daily Yoghurts</h1>
+            </header>
+            <Navbar/>
+            <Routes>
+              <Route path='/' element={<Getproducts/>}/>
+              <Route path='/signin' element={<Signin/>}/>
+              <Route path='/signup' element={<Signup/>}/>
+              <Route path='/addproducts' element={<Addproducts/>}/>
+            </Routes>
+          </div>
+        </Router>
   );
 }
 
